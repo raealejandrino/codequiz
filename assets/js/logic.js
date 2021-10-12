564f6efc5b8971d5bd0868e99370e16ad860600e
Rae Alejandrino <raealejandrino@gmail.com>
2021-10-12 09:51:37 -0400

    Successfully display dynamically created elemented in js onto DOM

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,10 +6,15 @@ var deleteStartBtn = function() {
     startButtonEl.remove();
     console.log("Success");
     spawnQuestion();
+    spawnAnswer();
 }
 
 var questionA = {
-    question: "Insert question here?"
+    question: "Insert question here?",
+    answer1: "A",
+    answer2: "B",
+    answer3: "C",
+    answer4: "D"
 };
 
 // create question with next button
@@ -31,7 +36,30 @@ var spawnQuestion = function() {
 var spawnAnswer = function() {
     var answerContainerEl = document.createElement("div");
     answerContainerEl.className = "quizAnswer";
-    
+
+    var answerOne = document.createElement("button");
+    answerOne.className = "answerButton";
+    answerOne.innerHTML = questionA.answer1;
+    answerContainerEl.appendChild(answerOne);
+
+    var answerTwo = document.createElement("button");
+    answerTwo.className = "answerButton";
+    answerTwo.innerHTML = questionA.answer2;
+    answerContainerEl.appendChild(answerTwo);
+
+    var answerThree = document.createElement("button");
+    answerThree.className = "answerButton";
+    answerThree.innerHTML = questionA.answer3;
+    answerContainerEl.appendChild(answerThree);
+
+    var answerFour = document.createElement("button");
+    answerFour.className = "answerButton";
+    answerFour.innerHTML = questionA.answer4;
+    answerContainerEl.appendChild(answerFour);
+
+    mainQuizSection.appendChild(answerContainerEl);
+
+
 }
 
 startButtonEl.addEventListener('click', deleteStartBtn);
